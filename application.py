74c2d976f4f0133b76cce925490d257d3aa009db
MajorTom97 <cesargalileo1969@gmail.com>
2021-09-23 23:20:15 -0600

    applicationpy crasheada :(

--- application.py
+++ application.py
@@ -35,30 +35,34 @@ def login():
     if request.method == "POST":
 
         # Ensure username was submitted
+        user_conf = db.execute("SELECT * FROM users WHERE username = :username",{"username": username}).fetchone()
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return render_template("error_apology.html", message="The username does not exist")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return render_template("error_apology.html", message="Something went wrong with the password, check it!")
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username",
                           username=request.form.get("username"))
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        if not len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("error_apology.html", message="Something went wrong with the password, check it!")
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = user_conf[0]
+        session["username"] = user_conf[1]
+        session["logged_up"] = True
 
         # Redirect user to home page
+        flash ("You were looged up!!!")
         return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("login.html")
+        return render_template("login.html", message="Login here!!!")
 
 @app.route("/logout")
 def logout():
@@ -68,7 +72,7 @@ def logout():
     session.clear()
 
     # Redirect user to login form
-    return redirect("/")
+    return redirect(url_for("login"))
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -76,50 +80,53 @@ def register():
 
     # Olvidar cualquier registro anterior!
     session.clear()
+    username = request.form.get("username")
+    password = request.form.get("password")
 
+    if user_conf = db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchall()
     # Asegurarse de recepcionar datos por formulario!
     if request.method == "POST":
 
         # Si no se introduce usuario, enviar mensaje de error!
         if not request.form.get("username"):
-            return apology("Debes Ingresar un Usuario", 400)
+            return render_template("error_apology.html", message="This user alredy exists!")
 
         # Si no se ingresa una cotraseña, enviar mensaje de error!
         elif not request.form.get("password"):
-            return apology("Ingresar una Contraseña", 400)
+            return render_template("error_apology.html", message="You must give a password")
 
         # Confirmacion de Contraseña
         elif not request.form.get("confirmation"):
-            return apology("Contraseñas no coinciden!", 400)
+            return render_template("error_apology.html", message="Opsss! It seems that you forget your password")
 
         # Si las contraseñas no coinciden, enviar mensaje de error!
         elif not request.form.get("confirmation") == request.form.get("password"):
-            return apology("Contraseña Incorrecta", 400)
+            return render_template("error_apology.html", message="Password does not match!")
         # Hash de Contraseña
         hash_contraseña = generate_password_hash(request.form.get("password"))
 
         res = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         if len(res) != 0:
-            return apology("Este usuario ya existe!")
+            return render_template("error_apology.html", message="The user ")
 
-        nuevo_idUsuario = db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)",
+        new_user = db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)",
                                      username=request.form.get("username"),
                                      hash=hash_contraseña)
 
-        session["user_id"] = nuevo_idUsuario
+        session["user_id"] = new_user
 
-        if not nuevo_idUsuario:
-            return apology("Ya existe este Usuario!")
+        if not new_user:
+            return render_template("error_apology.html", message="This user already exists! Try it with another username")
 
-        flash("Registro Exitoso!")
+        flash("Success!")
 
         # Retornar pagina de Inicio
-        return redirect(url_for("index"))
+        return redirect(url_for("login"))
 
     # Retornar a plantilla register, al cual usuario accede al hacer click en button register
     else:
-        return render_template("register.html")
+        return render_template("register.html", message="Please register over here!")
 
 @app.route("/")
 def index():
@@ -127,12 +134,23 @@ def index():
 
     return render_template("index.html")
 
-# return jsonify({
-#                 "title":title,
-#                 "author":author,
-#                 "isbn":isbn,
-#                 "year":year,
-#                 "review_count":"No contiene",
-#                 "average_score":"No contiene"
-#             })
-# href=/libro/{{libro.id}}
+@app.route("/api/<text:isbn>", methods=["GET"])
+def api_book(isbn):
+
+    """Call the Api from google books"""
+    if request.method == 'GET':
+        book=db.execute("SELECT * FROM books_list WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
+    
+    if not book:
+        return jsonify('Ohh Ohhh We did not found it!'), 403
+    
+    data = jsonify({
+        "title": book;
+        "author": book;
+        "year":
+        "isbn":
+        "review":
+        "points":
+    })
+
+    return data, 200
